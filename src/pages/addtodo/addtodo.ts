6a3d4d13d81d76bcff824e88ec5c04f31d0a2e0d
Arnon Puitrakul <peter_arnon1@icloud.com>
2017-03-17 14:30:58 +0700

    Add New Todo Page

---
+++ src/pages/addtodo/addtodo.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'addtodo',
+  templateUrl: 'addtodo.html'
+})
+export class AddTodoPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
