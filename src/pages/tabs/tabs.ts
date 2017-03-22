2250d369c1076361032e6a53fe1119ce50fd3cef
Arnon Puitrakul <peter_arnon1@icloud.com>
2017-03-22 15:59:59 +0700

    Change Tab Buttons Target

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 import { HomePage } from '../home/home';
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
+import { CategoryPage } from '../category/category';
+import { ProfilePage } from '../profile/profile';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -11,8 +11,8 @@ export class TabsPage {
   // this tells the tabs component which Pages
   // should be each tab's root Page
   tab1Root: any = HomePage;
-  tab2Root: any = AboutPage;
-  tab3Root: any = ContactPage;
+  tab2Root: any = CategoryPage;
+  tab3Root: any = ProfilePage;
 
   constructor() {
 
