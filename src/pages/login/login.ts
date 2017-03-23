bb85b65d002d771228f9a98fef2f63935a9b7366
Arnon Puitrakul <peter_arnon1@icloud.com>
2017-03-23 14:16:28 +0700

    Create Navigation Function for Login Button

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController} from 'ionic-angular';
+import { TabsPage } from '../tabs/tabs';
 
 /*
   Generated class for the Login page.
@@ -13,10 +14,15 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+  }
+
+  login ()
+  {
+    this.navCtrl.push(TabsPage)
   }
 
 }
