a3bf2d17e566ea8c486ce4c74f21996c91064c63
Arnon Puitrakul <peter_arnon1@icloud.com>
2017-05-03 02:14:06 +0700

    Add AuthProviders

--- src/app/providers/auth-provider.ts
+++ src/app/providers/auth-provider.ts
@@ -1,93 +1,37 @@
-import { Injectable, EventEmitter, Inject } from '@angular/core';
-import { AuthProviders, AngularFire, FirebaseAuthState, AuthMethods, FirebaseApp } from 'angularfire2'; //Add FirebaseApp
-import { Observable } from "rxjs/Observable";
-import { Platform } from 'ionic-angular';
-import { Facebook } from 'ionic-native';
-import { auth } from 'firebase'; //needed for the FacebookAuthProvider
+import { Injectable } from '@angular/core';
+import { AuthProviders, AngularFireAuth, FirebaseAuthState, AuthMethods } from 'angularfire2';
 
 @Injectable()
-export class AuthProvider {
+export class AuthService {
   private authState: FirebaseAuthState;
-  public onAuth: EventEmitter<FirebaseAuthState> = new EventEmitter();
-  public firebase : any;
 
-  constructor(private af: AngularFire, @Inject(FirebaseApp)firebase: any,private platform: Platform) { //Add reference to native firebase SDK
-    this.firebase = firebase;
-    this.af.auth.subscribe((state: FirebaseAuthState) => {
+  constructor(public auth$: AngularFireAuth) {
+    this.authState = auth$.getAuth();
+    auth$.subscribe((state: FirebaseAuthState) => {
       this.authState = state;
-      this.onAuth.emit(state);
     });
   }
 
-  loginWithFacebook() {
-    return Observable.create(observer => {
-      if (this.platform.is('cordova')) {
-        return Facebook.login(['email', 'public_profile']).then(res => {
-          const facebookCredential = auth.FacebookAuthProvider.credential(res.authResponse.accessToken);
-          this.firebase.auth().signInWithCredential(facebookCredential).then(()=>{
-            observer.next();
-          }).catch(error => {
-            //console.log(error);
-            observer.error(error);
-          });
-        });
-      } else {
-        return this.af.auth.login({
-          provider: AuthProviders.Facebook,
-          method: AuthMethods.Popup
-          }).then(()=>{
-            observer.next();
-          }).catch(error => {
-            //console.log(error);
-            observer.error(error);
-        });
-      }
+  get authenticated(): boolean {
+    return this.authState !== null;
+  }
+
+  signInWithFacebook(): firebase.Promise<FirebaseAuthState> {
+    return this.auth$.login({
+      provider: AuthProviders.Facebook,
+      method: AuthMethods.Popup
     });
   }
 
-  loginWithEmail(credentials) {
-    return Observable.create(observer => {
-      this.af.auth.login(credentials, {
-        provider: AuthProviders.Password,
-        method: AuthMethods.Password
-      }).then((authData) => {
-        //console.log(authData);
-        observer.next(authData);
-      }).catch((error) => {
-        observer.error(error);
-      });
-    });
+  signOut(): void {
+    this.auth$.logout();
   }
 
-  registerUser(credentials: any) {
-    return Observable.create(observer => {
-      this.af.auth.createUser(credentials).then(authData => {
-        //authData.auth.updateProfile({displayName: credentials.displayName, photoURL: credentials.photoUrl}); //set name and photo
-        observer.next(authData);
-      }).catch(error => {
-        //console.log(error);
-        observer.error(error);
-      });
-    });
-  }
-
-  resetPassword(emailAddress:string){
-    return Observable.create(observer => {
-      this.firebase.auth().sendPasswordResetEmail(emailAddress).then(function(success) {
-          //console.log('email sent', success);
-          observer.next(success);
-        }, function(error) {
-          //console.log('error sending email',error);
-          observer.error(error);
-        });
-     });
-  }
-
-  logout() {
-    this.af.auth.logout();
-  }
-
-  get currentUser():string{
-    return this.authState?this.authState.auth.email:'';
+  displayName(): string {
+    if (this.authState != null) {
+      return this.authState.facebook.displayName;
+    } else {
+      return '';
+    }
   }
 }
