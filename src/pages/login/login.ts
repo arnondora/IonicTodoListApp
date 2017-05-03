d9ca842bc46ac21d3a64bd9944a7e9808e33280b
Arnon Puitrakul <peter_arnon1@icloud.com>
2017-05-03 21:58:45 +0700

    Remove Unused Code

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -2,23 +2,14 @@ import { Component } from '@angular/core';
 import { NavController} from 'ionic-angular';
 import { TabsPage } from '../tabs/tabs';
 import { AuthService } from '../../providers/auth-service';
-import { AngularFire, FirebaseListObservable} from 'angularfire2';
 
-/*
-  Generated class for the Login page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
 })
 export class LoginPage {
 
-  items: FirebaseListObservable<any[]>;
-
-  constructor(public navCtrl: NavController, public af: AngularFire, private _auth: AuthService) {}
+  constructor(public navCtrl: NavController, private _auth: AuthService) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
