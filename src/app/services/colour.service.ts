05aae35d0f880ace37ab460398ff1f89bf891686
Arnon Puitrakul <peter_arnon1@icloud.com>
2017-05-03 20:42:06 +0700

    Sync Colour with ColourService

--- src/app/services/colour.service.ts
+++ src/app/services/colour.service.ts
@@ -7,7 +7,20 @@ export class ColourService
         this.color = [
             { name : "Red", code : "red"},
             { name : "Pink", code : "pink"},
+            { name : "Purple", code : "purple"},
+            { name : "Deep Purple", code : "deeppurple"},
+            { name : "Indigo", code : "indigo"},
             { name : "Blue", code : "blue"},
+            { name : "Lightblue", code : "Lightblue"},
+            { name : "Cyan", code : "cyan"},
+            { name : "Teal", code : "teal"},
+            { name : "Green", code : "green"},
+            { name : "Light Green", code : "lightgreen"},
+            { name : "Lime", code : "lime"},
+            { name : "Yellow", code : "yellow"},
+            { name : "Orange", code : "orange"},
+            { name : "Deep Orange", code : "deeporange"},
+            { name : "Brown", code : "brown"},
         ];
     }
 
