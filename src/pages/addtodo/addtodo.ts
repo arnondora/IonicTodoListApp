21c704cbb00b04dc17fb5306b610ddf8b3afda22
Arnon Puitrakul <peter_arnon1@icloud.com>
2017-05-03 17:18:27 +0700

    Add Add Todo Feature

--- src/pages/addtodo/addtodo.ts
+++ src/pages/addtodo/addtodo.ts
@@ -2,14 +2,47 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import { AngularFire, FirebaseListObservable} from 'angularfire2';
+
 @Component({
   selector: 'addtodo',
   templateUrl: 'addtodo.html'
 })
 export class AddTodoPage {
 
-  constructor(public navCtrl: NavController) {
+  private uid: string;
+  private categories : Array<any>;
+  private categoryList: FirebaseListObservable<any>;
+  private todo = {};
 
+  constructor(public navCtrl: NavController, public af: AngularFire) {
+    this.initUserProfile();
+    this.initCategoryList();
+  }
+
+  initUserProfile ()
+  {
+      this.af.auth.subscribe(auth => {
+        this.uid = auth.uid;
+      });
+  }
+
+  initCategoryList ()
+  {
+    this.af.database.list('/users/' + this.uid + '/categories', { preserveSnapshot: true})
+      .subscribe(snapshots=>{
+          this.clearList();
+          snapshots.forEach(snapshot => {
+            this.categories.push({
+              name: snapshot.val()['name']
+            });
+         });
+      });
+  }
+
+  clearList ()
+  {
+    this.categories = Array();
   }
 
   gotoHome ()
@@ -20,8 +53,14 @@ export class AddTodoPage {
   submitTodo ()
   {
     //Save the todo here !
-    
-    this.gotoHome();
+  this.af.database.list('/users/' + this.uid + '/categories/' + this.todo['category'] + '/todos').push({
+      name : this.todo['name'],
+      timestamp: Date.now(),
+    }).then (newCategory => {
+      this.gotoHome();
+    }, error => {
+      console.log(error)
+    });
   }
 
 }
