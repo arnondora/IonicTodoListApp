48eb1ae4ece874a6e6c4b6758b14459ce8bfb343
Arnon Puitrakul <peter_arnon1@icloud.com>
2017-05-03 17:36:00 +0700

    Add Show Category in Category Home

--- src/pages/categoryhome/categoryhome.ts
+++ src/pages/categoryhome/categoryhome.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import { AngularFire} from 'angularfire2';
+
 /*
   Generated class for the Category page.
 
@@ -14,13 +16,49 @@ import { NavController, NavParams } from 'ionic-angular';
 export class CategoryHomePage {
   private catName:String;
   private catColour:String;
-  private todos: any;
+  private todos: Array<any>;
+  private uid: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) 
+  constructor(public navCtrl: NavController, public navParams: NavParams, public af: AngularFire)
   {
+    //Get Data From Previous Page
     this.catName = navParams.get('name').charAt(0).toUpperCase() + navParams.get('name').slice(1);
     this.catColour = navParams.get('colour');
-    this.initDumpData();
+
+    this.initUserProfile();
+    this.initDataFromDB();
+  }
+
+  //Page Life Cycle
+  ionViewDidLoad() {
+    console.log(this.catName + ' Page has been loaded');
+  }
+
+
+  initUserProfile ()
+  {
+      this.af.auth.subscribe(auth => {
+        this.uid = auth.uid;
+      });
+  }
+
+  initDataFromDB ()
+  {
+    this.af.database.list('/users/' + this.uid + '/categories/' + this.catName + '/todos', { preserveSnapshot: true})
+      .subscribe(snapshots=>{
+          this.clearList();
+          snapshots.forEach(snapshot => {
+            this.todos.push({
+              body: snapshot.val()['name'],
+              category: this.catName
+            });
+         });
+      });
+  }
+
+  clearList ()
+  {
+    this.todos = Array();
   }
 
   initDumpData ()
@@ -29,10 +67,6 @@ export class CategoryHomePage {
       {body: "Buy Milk" , category : "Home"},
       {body: "Buy Dinner", category: "Home"}
     ];
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CategoryPage');
   }
 
 }
